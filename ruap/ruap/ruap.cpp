bd124cb366521de898b97fa3d45fd302aecb4384
Andi <abasic@etfos.hr>
2017-03-23 09:21:01 +0100

    Prvi commit

---
+++ ruap/ruap/ruap.cpp
@@ -0,0 +1,15 @@
+// ruap.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Pozdrav iz aplikacije od Armanda Vlajcica!" << endl;
+	return 0;
+}
+
